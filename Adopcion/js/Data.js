eb95f58fbf86038b1e60f21d796efb1f956eba36
Carlos Daniel Rico Galindo <82829219+Rico0308@users.noreply.github.com>
2021-05-03 23:29:07 -0500

    Update Data.js

--- Adopcion/js/Data.js
+++ Adopcion/js/Data.js
@@ -259,13 +259,13 @@ mensajes = [
     mensaje: "Buenas días, estoy interesado en adoptar a la gatita Pelusin"
   },
   {
-    img: "../img/avatar3.png",
+    img: "./img/avatar3.png",
     nombre: "María Martínez",
     hora: "2:00 PM",
     mensaje: "Buenas tardes, estoy muy interesada en adoptar al gatito Copito"
   },
   {
-    img: "../img/avatar4.png",
+    img: "./img/avatar4.png",
     nombre: "Jonás Pérez",
     hora: "9:00 AM",
     mensaje: "Buenos días, me gustaría adoptar al perrito Bob"
